8f493317d78b0601cfee27c27b60bcd375be6ee4
jasiuxx <155495828+jasiuxx@users.noreply.github.com>
2025-06-15 12:44:35 +0200

    Update settings.py

--- wydruki_web_api-main/magazyn_config/settings.py
+++ wydruki_web_api-main/magazyn_config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fuyef3!w5y7!fi)82m%3c!%zg&e*)imujwjh!e20b=%hd56=0#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.100','192.168.1.13']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Konfiguracja ścieżki sieciowej do produktów - wyłączona
